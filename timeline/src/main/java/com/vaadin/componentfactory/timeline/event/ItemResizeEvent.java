a5f134c1c5b6bc2767f890328a876d8f257c55fe
Paola De Bartolo <paola.debartolo@gmail.com>
2021-10-06 17:32:59 -0300

    refactor: move events to separate classes

---
+++ timeline/src/main/java/com/vaadin/componentfactory/timeline/event/ItemResizeEvent.java
@@ -0,0 +1,71 @@
+/*-
+ * #%L
+ * Timeline
+ * %%
+ * Copyright (C) 2021 Vaadin Ltd
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.vaadin.componentfactory.timeline.event;
+
+import com.vaadin.componentfactory.timeline.Timeline;
+import com.vaadin.flow.component.ComponentEvent;
+import java.time.LocalDateTime;
+
+/** 
+ * Event thrown when an item is resized. 
+ */
+public class ItemResizeEvent extends ComponentEvent<Timeline> {
+
+  private final String itemId;
+  private final LocalDateTime newStart;
+  private final LocalDateTime newEnd;
+  private boolean cancelled = false;
+
+  public ItemResizeEvent(
+      Timeline source,
+      String itemId,
+      LocalDateTime newStart,
+      LocalDateTime newEnd,
+      boolean fromClient) {
+    super(source, fromClient);
+    this.itemId = itemId;
+    this.newStart = newStart;
+    this.newEnd = newEnd;
+  }
+
+  public String getItemId() {
+    return itemId;
+  }
+
+  public LocalDateTime getNewStart() {
+    return newStart;
+  }
+
+  public LocalDateTime getNewEnd() {
+    return newEnd;
+  }
+
+  public boolean isCancelled() {
+    return cancelled;
+  }
+
+  public void setCancelled(boolean cancelled) {
+    this.cancelled = cancelled;
+  }
+
+  public Timeline getTimeline() {
+    return (Timeline) source;
+  }
+}
