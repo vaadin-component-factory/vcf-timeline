0782bcf4650a4e434433dfdd2de61b33105cc094
Paola De Bartolo <paola.debartolo@gmail.com>
2021-10-06 17:18:54 -0300

    feat: add new event for drag and drop to handle multi selection
    Close #4

--- timeline-demo/src/main/java/com/vaadin/componentfactory/timeline/DragAndDropItemsExample.java
+++ timeline-demo/src/main/java/com/vaadin/componentfactory/timeline/DragAndDropItemsExample.java
@@ -7,7 +7,8 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Random;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Route(value = "drag-and-drop-items", layout = MainLayout.class)
 public class DragAndDropItemsExample extends Div {
@@ -43,8 +44,17 @@ public class DragAndDropItemsExample extends Div {
     item3.setId(3);
     item3.setEditable(true);
     item3.setUpdateTime(true);
+    
+    Item item4 =
+        new Item(
+            LocalDateTime.of(2021, 8, 16, 1, 30, 00),
+            LocalDateTime.of(2021, 8, 17, 1, 00, 00),
+            "Item 4");
+    item4.setId(4);
+    item4.setEditable(true);
+    item4.setUpdateTime(true);
 
-    Item[] items = new Item[] {item1, item2, item3};
+    Item[] items = new Item[] {item1, item2, item3, item4};
 
     // empty timeline creation
     Timeline timeline = new Timeline(items);
@@ -57,29 +67,40 @@ public class DragAndDropItemsExample extends Div {
     timeline.setMultiselect(true);
 
     // add listener to get new range values for drag item(s)
-    timeline.addItemMoveListener(
+    timeline.addItemsDragAndDropListener(
         e -> {
-          e.setCancelled(new Random().nextBoolean());
+          e.setCancelled(cancelMove(e.getItems()));
           if (e.isCancelled()) {
+            if(e.getItems().size() == 1) {
+              log.add(
+                  new Span("Moving item " + e.getItems().get(0).getContent() + " is not allowed. Moving reverted."));
+            }
             log.add(
-                new Span("Moving item " + e.getItemId() + " is not possible. Moving reverted."));
+                new Span("Moving items " + e.getItems().stream().map(Item::getContent).collect(Collectors.joining(",")) + " is not allowed. Moving reverted."));
           } else {
-            log.add(
-                new Span(
+            
+            for(Item item: e.getItems()) {
+              log.add(                  
+                  new Span(
                     "Item: "
-                        + e.getItemId()
+                        + item.getContent()
                         + " was dragged. New start: "
-                        + formatDates(e.getNewStart())
+                        + formatDates(item.getStart())
                         + " - New end: "
-                        + formatDates(e.getNewEnd())));
+                        + formatDates(item.getEnd())));
+            }
           }
         });
 
     timeline.setZoomable(false);
 
-    add(timeline, log);
+    add(timeline, log);    
   }
 
+  private boolean cancelMove(List<Item> items) {
+    return items.stream().anyMatch(i -> i.getStart().getHour() == 2 && i.getStart().getMinute() == 30);
+  }
+  
   private String formatDates(LocalDateTime date) {
     return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss"));
   }
