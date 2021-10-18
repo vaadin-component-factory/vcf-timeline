93b8f16efeceda42290ad09bbda89a17d962e4e7
Martín López <mlopez@flowingcode.com>
2021-10-18 18:27:47 -0300

    fix: make autoscrolling stop on edges

--- timeline/src/main/resources/META-INF/resources/frontend/src/vcf-timeline.js
+++ timeline/src/main/resources/META-INF/resources/frontend/src/vcf-timeline.js
@@ -55,7 +55,7 @@ window.vcftimeline = {
 			var range = container.timeline._timeline.getWindow();
 			var widthInPixels = container.timeline._timeline.body.domProps.lastWidth;
 			var widthInMilliseconds = range.end.valueOf() - range.start.valueOf();
-			if(item.data.start <= range.start) {
+			if(item.data.start <= range.start && range.start > new Date(options.min)) {
 				container.timeline._timeline.setWindow(
 					new Date(range.start.valueOf() - (widthInMilliseconds / 50)),
 					new Date(range.end.valueOf() - (widthInMilliseconds / 50)),
@@ -64,7 +64,7 @@ window.vcftimeline = {
 				container.timeline._timeline.itemSet.touchParams.itemProps[0].initialX = ix + (widthInPixels / 50);
 				item.data.start = new Date(item.data.start.valueOf() - (widthInMilliseconds / 50));
 				item.data.end = new Date(item.data.end.valueOf() - (widthInMilliseconds / 50));
-			} else if(item.data.end >= range.end) {
+			} else if(item.data.end >= range.end && range.end < new Date(options.max)) {
 				container.timeline._timeline.setWindow(
 					new Date(range.start.valueOf() + (widthInMilliseconds / 50)),
 					new Date(range.end.valueOf() + (widthInMilliseconds / 50)),
